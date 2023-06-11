fd0c4b7aa6f59e31203eb6b34ca4e02edc354073
Tanishq <tanishq.jaiswal97@gmail.com>
2023-06-11 14:01:39 +0530

    5

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
